a1034b45283da7341c31957084e3d048e0bbf3c8
Christian Drappi <christiandrappi@gmail.com>
2012-05-18 11:24:31 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeVoteHere::Application.config.secret_token = 'dae72e35a5d417167aba891b903a30d949588dbc108a781f26ba60c45a450a40bd05761756b106ada0bbbce71ff0475c1885dd5f2e85f1f52e4e4a930549f4ab'
