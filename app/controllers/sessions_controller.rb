5b22907258b4ebae1ec64d47f39d8fc2f99fc492
Christian Drappi <christiandrappi@gmail.com>
2012-05-20 23:22:02 -0400

    added ability to log in with handles or email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,20 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:session][:email])
+		if params[:session][:email].include? "@"
+			user = User.find_by_email(params[:session][:email])
+		else
+			user = User.find_by_handle(params[:session][:email])
+		end
+
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_back_or user
 		elsif user
-			flash.now[:error] = 'Invalid email'
+			flash.now[:error] = 'Invalid login/password combination'
 			render 'new'
 		else
-			flash.now[:error] = 'Invalid email/password combination'
+			flash.now[:error] = 'Invalid email or handle'
 			render 'new'
 		end
 	end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(params[:user])
+    if params[:user][:handle].include? "@"
+      flash.now[:error] = "Handle cannot contain an '@' symbol"
+      render 'edit'
+    elsif @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
       redirect_to @user
