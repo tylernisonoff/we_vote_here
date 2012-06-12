40f6ff48d826c1fb304292ee54825068ea946a2c
Christian Drappi <christiandrappi@gmail.com>
2012-06-12 18:10:26 -0400

    commented out a function in preferences controller

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -2,12 +2,12 @@ class PreferencesController < ApplicationController
 
 	belongs_to :vote, dependent: :allow_destroy
 
-	def sort
-    	Preference.all.each do |preference|
-      		if position = params[:preferences].index(preference.id.to_s)
-        		preference.update_attribute(:position, position + 1) unless spec.position == position + 1
-      		end
-    	end
-    	render nothing: true, :status => 200
-  end
+	# def sort
+ #    	Preference.all.each do |preference|
+ #      		if position = params[:preferences].index(preference.id.to_s)
+ #        		preference.update_attribute(:position, position + 1) unless spec.position == position + 1
+ #      		end
+ #    	end
+ #    	render nothing: true, :status => 200
+ #  end
 end
