f50753342cd9e6626c2fd6b168e94da3827069e5
Christian Drappi <christiandrappi@gmail.com>
2012-08-30 20:25:43 -0400

    Test Commit on ChrUbuntu

--- app/models/candidate.rb
+++
@@ -1,9 +0,0 @@
-class Candidate < ActiveRecord::Base
-  attr_accessible :name, :question_id
-
-  belongs_to :question
-
-  validates :name, presence: true, length: { minimum: 1, message: "Candidates cannot be blank"}
-  validates_uniqueness_of :name, scope: [:question_id], message: "Candidates cannot have the same name"
-
-end
