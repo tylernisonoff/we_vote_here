b85916c30c8b98d6c0f51283bdf057c9e2dff847
Christian Drappi <christiandrappi@gmail.com>
2012-09-05 09:30:17 -0400

    moved smtp settings back to env.rb from env/dev.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,10 @@ WeVoteHere::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+  # Make sure Rails doesn't raise errors on mail delivery:
+  config.action_mailer.raise_delivery_errors = false
+
+
 end
