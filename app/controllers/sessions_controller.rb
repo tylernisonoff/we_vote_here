df41814adf397c9ba8715ad9c6ff6482b8b126a1
Christian Drappi <christiandrappi@gmail.com>
2012-09-24 04:00:05 -0400

    fixed sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,15 @@ class SessionsController < ApplicationController
 		if UserEmail.exists?(email: params[:session][:email])
 			email = UserEmail.find_by_email(params[:session][:email])
 			user = email.user
+			if user.authenticate(params[:session][:password])
+				sign_in user
+				redirect_back_or root_path
+			else
+				flash.now[:error] = 'Invalid login/password combination'
+				redirect_back_or signin_path
+			end
 		else
 			flash.now[:error] = 'Invalid email'
-			redirect_back_or signin_path
-		end
-
-		if user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_back_or root_path
-		else
-			flash.now[:error] = 'Invalid login/password combination'
 			redirect_back_or signin_path
 		end
 	end
