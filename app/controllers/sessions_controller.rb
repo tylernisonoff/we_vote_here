2132ac1c1764d71cdeca52f55415600663e6f809
Christian Drappi <christiandrappi@gmail.com>
2012-09-24 03:51:23 -0400

    hopefully fixed heroku signin issues once and for all

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,19 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		email = UserEmail.find_by_email(params[:session][:email])
-		user = email.user
-
-		if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_back_or root_path
-		elsif user
-			flash.now[:error] = 'Invalid login/password combination'
-			redirect_back_or signin_path
+		if UserEmail.find_by_email(params[:session][:email])
+			email = UserEmail.find_by_email(params[:session][:email])
+			user = email.user
 		else
 			flash.now[:error] = 'Invalid email'
+			redirect_back_or signin_path
+		end
+
+		if user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_back_or root_path
+		else
+			flash.now[:error] = 'Invalid login/password combination'
 			redirect_back_or signin_path
 		end
 	end
